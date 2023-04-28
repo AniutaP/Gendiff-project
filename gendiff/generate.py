d0599eaca541cdba9e29a36c696a8401e54ba0e0
anna_petukhova <aniuta.petuhova2013@yandex.ru>
2023-04-28 20:03:25 +0300

    fix generate

--- gendiff/generate.py
+++ gendiff/generate.py
@@ -4,21 +4,21 @@ import itertools
 
 
 def generate_diff(first_file, second_file):
-    file_path1_data = json.load(first_file)
-    file_path2_data = json.load(second_file)
+    file1_data = json.load(first_file)
+    file2_data = json.load(second_file)
     data = []
-    for key in file_path1_data:
-        if key not in file_path2_data:
-            data.append(f' - {key}: {file_path1_data[key]}')
-        elif file_path1_data[key] == file_path2_data[key]:
-            data.append(f'   {key}: {file_path1_data[key]}')
-        elif file_path1_data[key] != file_path2_data[key]:
-            data.append(f' - {key}: {file_path1_data[key]}')
-            data.append(f' + {key}: {file_path2_data[key]}')
-    for key in file_path2_data:
-        if key not in file_path1_data:
-            data.append(f' + {key}: {file_path2_data[key]}')
-    result = "\n".join(sorted(data, key=lambda x: x[3]))
-    print('{')
-    print(result)
-    print('}')
+    for key in file1_data:
+        if key not in file2_data:
+            data.append(f' - {key}: {file1_data[key]}')
+        elif file1_data[key] == file2_data[key]:
+            data.append(f'   {key}: {file1_data[key]}')
+        else:
+            data.append(f' - {key}: {file1_data[key]}')
+            data.append(f' + {key}: {file2_data[key]}')
+    for key in file2_data:
+        if key not in file1_data:
+            data.append(f' + {key}: {file2_data[key]}')
+
+    result = itertools.chain("{", sorted(data, key=lambda x: x[3]), "}")
+    print('\n'.join(result))
+    return '\n'.join(result)
