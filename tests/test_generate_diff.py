1de251e9012f368e174c2b814a6ba72431b44233
anna_petukhova <aniuta.petuhova2013@yandex.ru>
2023-05-06 02:55:40 +0300

    fix

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,6 +1,10 @@
 from gendiff.generate import generate_diff
-from tests.fixtures.json_right_result import JSON_RESULT
+from tests.fixtures.right_result import JSON_RESULT, YML_RESULT
 
 
-def test_generate_diff():
+def test_generate_diff_json():
     assert generate_diff('tests/fixtures/filepath1.json', 'tests/fixtures/filepath2.json') == JSON_RESULT
+
+
+def test_generate_diff_yml():
+    assert generate_diff('tests/fixtures/filepath1.yml', 'tests/fixtures/filepath2.yml') == YML_RESULT
