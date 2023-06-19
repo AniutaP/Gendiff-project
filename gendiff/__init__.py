3ff81bfc036bbb01c49a364a441cba6ac4050bde
anna_petukhova <aniuta.petuhova2013@yandex.ru>
2023-06-20 02:07:39 +0300

    fix read_files

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from gendiff.generate import generate_diff, get_diff
 from gendiff.parse_args import make_parse_args
-from gendiff.read_files import read_file
+from gendiff.read_files import read_file, find_file_type
 from gendiff.formatter.stylish import stylish, make_stylish, stringify
 from gendiff.formatter.format import get_format
 from gendiff.formatter.plain import plain, make_plain, to_string
@@ -19,5 +19,6 @@ __all__ = ('generate_diff',
            'plain',
            'make_plain',
            'to_string',
-           'json_format'
+           'json_format',
+           'find_file_type',
            )
--- gendiff/read_files.py
+++ gendiff/read_files.py
@@ -6,6 +6,18 @@ from yaml.loader import SafeLoader
 
 def read_file(some_file):
     if some_file[-4:] == ".yml" or some_file[-5:] == ".yaml":
-        return yaml.load(open(some_file), Loader=SafeLoader)
-    if some_file[-5:] == ".json":
-        return json.load(open(some_file))
+        file_name = yaml
+    elif some_file[-5:] == ".json":
+        file_name = json
+    else:
+        raise ValueError('Unknown format file')
+
+    file_data = open(some_file)
+    return find_file_type(file_name, file_data)
+
+
+def find_file_type(file_name, file_data):
+    if file_name == yaml:
+        return yaml.load(file_data, Loader=SafeLoader)
+    if file_name == json:
+        return json.load(file_data)
